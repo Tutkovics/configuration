0e180b99f1c0f71e10e59710032c083d8f287f36
Tutkovics <tutiandras@gmail.com>
2020-12-04 18:00:06 +0100

    Check package

--- script.py
+++ script.py
@@ -2,6 +2,9 @@ import yaml
 import logging
 import os
 
+def pyInit():
+    os.System("sudo pip3 install pyyaml")
+
 def readConfig(configFile):
     logging.info('Read configuartion from: ' + str(configFile))
     with open(configFile) as file:
@@ -14,8 +17,11 @@ def installApps(pacman, apps):
         os.system("sudo apt-get update")
         command = "sudo apt-get install {app} {flags}"
         for app in apps:
-            logging.info('Install app: ' + str(app))
-            os.system(command.format(app=app,flags="-y"))
+            if os.system("apt-cache show {app}".format(app=app)) == 0:
+                logging.info('Install app: ' + str(app))
+                os.system(command.format(app=app,flags="-y"))
+            else:
+                logging.warning("App: {app} not found!".format(app=app)))
         os.system("sudo apt-get autoremove")
     else:
         logging.error("Not supported packet manager")
@@ -24,6 +30,8 @@ def installApps(pacman, apps):
 def main():
     # configure logger
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+    # init python environment
+    pyInit()
     # read configuration
     conf = readConfig("./script_config.yaml")
     # install apps
